3f5eb5ab24816801dce191428b5b4f20eb50039b
HOTGU <gksrn42@gmail.com>
2021-08-19 17:25:26 +0900

    config/wsgi:application

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "udksi^h1njsa9+k(rpvnazx00sb#lyupo3+e7xeqf(#nlor0_k"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
